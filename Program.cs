8609853c805812555aecba23fbcf4d2020e249b5
jvillacres06 <123887195+jvillacres06@users.noreply.github.com>
2023-01-29 10:42:51 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_Log4Net
+{
+     class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void Main(string[] args)
+        {
+
+            log.Info("Mensaje de Información");
+
+            DivicionCero();
+            FormatoIncorrecto();
+
+
+            Console.ReadLine();
+
+            
+        }
+
+        private static void FormatoIncorrecto()
+        {
+            try
+            {
+                int num;
+                Console.Write("Ingrese un número: ");
+                
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                log.InfoFormat("Formato incorrecto");
+            }
+            
+        }
+
+        private static void DivicionCero()
+        {
+            int num1 = 7, num2 = 0, division = 0;
+
+            try
+            {
+                division = num1 / num2;
+                Console.WriteLine("Resultado: " + division);
+            }
+            catch (Exception)
+            {
+                log.Error("Nose puede dividir para cero");
+            }
+
+
+        }
+
+
+     }
+}
